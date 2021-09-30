7c1e7a5e301525f67f42196b9f1d5e9549b78094
Roman <mr.kostenko.roma@mail.ru>
2021-09-30 19:00:14 +0300

    Fetch photos

--- app/src/main/java/com/roman/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/roman/photogallery/FlickrFetchr.kt
@@ -1,11 +1,16 @@
 package com.roman.photogallery
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.BitmapFactory.decodeStream
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.roman.photogallery.api.FlickrApi
 import com.roman.photogallery.api.FlickrResponse
 import com.roman.photogallery.api.PhotoResponse
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -48,5 +53,13 @@ class FlickrFetchr {
         })
 
         return responseLiveData
+    }
+
+    @WorkerThread
+    fun fetchPhoto(url : String): Bitmap? {
+        val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
+        return bitmap
     }
 }
--- app/src/main/java/com/roman/photogallery/api/FlickrApi.kt
+++ app/src/main/java/com/roman/photogallery/api/FlickrApi.kt
@@ -1,7 +1,9 @@
 package com.roman.photogallery.api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
     @GET("services/rest/?method=flickr.interestingness.getList" +
@@ -10,4 +12,6 @@ interface FlickrApi {
     "&nojsoncallback=1" +
     "&extras=url_s")
     fun fetchPhotos(): Call<FlickrResponse>
+    @GET
+    fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
 }
